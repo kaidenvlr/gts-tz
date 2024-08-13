0b8dd4ecb7a30f07bf9b87d975e32adabea92869
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2024-08-13 18:36:42 +0500

    optimized imports

--- userapp/apps/posts/serializers.py
+++ userapp/apps/posts/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 
-from .models import Post, Category, Tag
+from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ("id", "title", "content", "category", "tags", "slug", "created_at")
-        depth = 1
