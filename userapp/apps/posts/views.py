fbf943d3a38876fcffad4272e6bc4f20e07dc1b4
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2024-08-13 18:36:51 +0500

    optimized imports, search and filter

--- userapp/apps/posts/views.py
+++ userapp/apps/posts/views.py
@@ -1,8 +1,7 @@
-from rest_framework import views
-from rest_framework.response import Response
-
 from apps.posts.models import Post
 from apps.posts.serializers import PostSerializer
+from rest_framework import views
+from rest_framework.response import Response
 
 
 class PostList(views.APIView):
@@ -17,11 +16,21 @@ class PostList(views.APIView):
 class PostFilter(views.APIView):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    filter_fields = ['tags', 'category']
 
     def get(self, request, *args, **kwargs):
-        queryset = Post.objects.filter(status=True, **request.query_params.dict())
+        tags = request.query_params.get('tags', "").split(',')
+        category = request.query_params.get('category', None)
+        if category and tags != ['']:
+            queryset = Post.objects.filter(category_id=category, tags__id__in=tags).distinct()
+        elif category:
+            queryset = Post.objects.filter(category_id=category)
+        elif tags != ['']:
+            queryset = Post.objects.filter(tags__id__in=tags).distinct()
+        else:
+            return Response({"detail": "Please provide tags or category."}, status=400)
         serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
 
 
 class SearchPost(views.APIView):
@@ -29,6 +38,9 @@ class SearchPost(views.APIView):
     queryset = Post.objects.filter(status=True)
 
     def get(self, request, *args, **kwargs):
-        queryset = Post.objects.filter(status=True, title__contains=request.query_params.get('title'))
+        title_container = request.query_params.get('title').lower()
+        if not title_container:
+            return Response({"title": "This field is required."}, status=400)
+        queryset = Post.objects.filter(title__icontains=title_container)
         serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
