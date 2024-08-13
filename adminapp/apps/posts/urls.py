2858fe984f6643ae9cb4f694ac068bb9fed2de87
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2024-08-13 18:54:22 +0500

    tags, categories: urls

--- adminapp/apps/posts/urls.py
+++ adminapp/apps/posts/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
     path('posts/filter/', views.PostFilter.as_view(), name='post-filter'),
     path('posts/search/', views.SearchPost.as_view(), name='search-post'),
+
+    path('tags/', views.TagList.as_view(), name='tag-list'),
+    path('tags/<int:pk>/', views.TagDetail.as_view(), name='tag-detail'),
+
+    path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
 ]
