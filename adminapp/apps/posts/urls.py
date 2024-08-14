35ccd0f2ce5e6a1e6af11ddcedff00c6db3f758d
Zhanibek Jumadiyev <84601413+kaidenvlr@users.noreply.github.com>
2024-08-14 13:23:08 +0500

    url names

--- adminapp/apps/posts/urls.py
+++ adminapp/apps/posts/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='post-list'),
-    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
-    path('posts/filter/', views.PostFilter.as_view(), name='post-filter'),
-    path('posts/search/', views.SearchPost.as_view(), name='search-post'),
+    path('posts/', views.PostList.as_view(), name='post_list'),
+    path('post/<str:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('posts/filter/', views.PostFilter.as_view(), name='post_filter'),
+    path('posts/search/', views.PostSearch.as_view(), name='post_search'),
 
-    path('tags/', views.TagList.as_view(), name='tag-list'),
-    path('tags/<int:pk>/', views.TagDetail.as_view(), name='tag-detail'),
+    path('tags/', views.TagList.as_view(), name='tag_list'),
+    path('tags/<int:pk>/', views.TagDetail.as_view(), name='tag_detail'),
 
-    path('categories/', views.CategoryList.as_view(), name='category-list'),
-    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
+    path('categories/', views.CategoryList.as_view(), name='category_list'),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category_detail'),
 ]
